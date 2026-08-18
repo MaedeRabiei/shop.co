9e9ecf196d3344f471e8f333e8b959452eeb2357
maede-28mra <maederabiei09@gmail.com>
2026-08-18 23:09:17 +0330

    hero section done

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ shopButton.addEventListener("click", () => {
     shopDropdown.classList.toggle("opacity-0");
     shopDropdown.classList.toggle("translate-y-2");
 });
+
+const logoSwiper = new Swiper(".logoSwiper", {
+    loop: true,
+
+    slidesPerView: 2,
+    spaceBetween: 40,
+
+    speed: 5000,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+
+        768: {
+            slidesPerView: 5,
+            spaceBetween: 60,
+        },
+
+        1024: {
+            slidesPerView: 7,
+            spaceBetween: 70,
+        },
+    },
+});
